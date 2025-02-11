4a201a70a4ed3082aa4ddf4537891264bae80491
Eduardo Guasti Ortiz <waterzide2000@gmail.com>
2025-02-11 10:26:36 -0500

    Added support for video, audio and image files

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -18,20 +18,83 @@ const MessageBubble = ({ message }) => {
     const messageUser = user;
     const messageContent = content;
 
+    const regexFile = /<attached:\s([\w-]+)\.(opus|webp|mp4|jpg)>/;
+    const matchFile = messageContent.match(regexFile);
+
     // Placeholder logic to determine message type
     let messageType = 'text';
-    console.log(`Message from ${messageUser} is of type ${messageType}`);
 
-    // Return the message bubble component
-    return (
-      <div
-        className="message-bubble"
-        data-date={date}
-        data-time={time}
-      >
-        <strong>{messageUser}</strong>: {messageContent}
-      </div>
-    );
+    if (matchFile) {
+      const [, fileName, fileType] = matchFile;
+
+      const completeFile = './files/' + fileName + '.' + fileType;
+
+      switch(fileType) {
+        case 'opus':
+          messageType = 'audio';
+          // Make a new audio message bubble
+          return (
+            <div
+              className="audio-bubble"
+              data-date={date}
+              data-time={time}
+            >
+              <audio
+                src={completeFile}
+                type={fileType}
+                controls="true"
+                autoPlay="false"
+                loop="false"
+              ></audio>
+            </div>
+          );
+        case 'jpg' || 'webp':
+          messageType = 'image';
+          // Make a new image message bubble
+          return (
+            <div
+              className="image-bubble"
+              data-date={date}
+              data-time={time}
+            >
+              <img
+                src={completeFile}
+                alt='image'
+              />
+            </div>
+          );
+        case 'mp4':
+          messageType = 'video';
+          // Make a new video message bubble
+          return (
+            <div
+              className="video-bubble"
+              data-date={date}
+              data-time={time}
+            >
+              <video
+                controls
+                src={completeFile}
+                type="video/mp4"
+              />
+            </div>
+          );
+      }
+      
+    } else {
+      console.log(`Message from ${messageUser} is of type ${messageType}`);
+
+      // Return the message bubble component
+      return (
+        <div
+          className="message-bubble"
+          data-date={date}
+          data-time={time}
+        >
+          <strong>{messageUser}</strong>: {messageContent}
+        </div>
+      );
+    }
   } else {
     // Handle messages that don't match the expected format
     console.warn('Message did not match expected format:', message);
